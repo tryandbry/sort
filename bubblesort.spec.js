f68fc4c6175ac9396fd08acfa932c84e577851b7
Bryan Kao <bryan.w.kao@gmail.com>
2017-04-20 11:20:30 -0500

    added test specs

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,7 +1,7 @@
 describe('Bubble Sort', function(){
 
   beforeEach(function() {
-    spyOn(toLower, 'call').and.callThrough();
+    spyOn(window,'toLower').and.callThrough();
   });
 
   it('handles an empty array', function(){
@@ -20,8 +20,14 @@ describe('Bubble Sort', function(){
 
   it('executes the expected number of times', function(){
     var arr = [3,1,2];
-    expect(toLower.calls.count()).toEqual(4);
-  }
+    bubbleSort(arr);
+    expect(toLower.calls.count()).toEqual(6);
+
+    arr = [4,1,3,2];
+    bubbleSort(arr);
+    expect(toLower.calls.count()).toEqual(18);
+
+  });
 });
 
 
