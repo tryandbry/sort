18352902941364e954d1117e1f7fa1457355d045
Bryan Kao <bryan.w.kao@gmail.com>
2017-04-20 11:12:01 -0500

    WIP

--- bubblesort.js
+++ bubblesort.js
@@ -4,14 +4,42 @@ function bubbleSort(array) {
   while(bSwitch) {
     bSwitch = false;
     for(var i=0;i<array.length;i++){
-      if(array[i] > array[i+1]) {
-	var buffer = array[i+1];
-	array[i+1] = array[i];
-	array[i] = buffer;
+      var buffer2 = array[i];
+      array[i] = toLower(array[i],array[i+1]);
+      if(array[i] != buffer2){
 	bSwitch = true;
+	array[i+1] = buffer2;
       }
     }
   }
 
   return array;
 }
+
+function toLower(a,b){
+  return a > b ? b : a;
+}
+
+
+
+
+
+
+
+//function bubbleSort(array) {
+//  var bSwitch = true;
+//
+//  while(bSwitch) {
+//    bSwitch = false;
+//    for(var i=0;i<array.length;i++){
+//      if(array[i] > array[i+1]) {
+//	var buffer = array[i+1];
+//	array[i+1] = array[i];
+//	array[i] = buffer;
+//	bSwitch = true;
+//      }
+//    }
+//  }
+//
+//  return array;
+//}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,28 @@
 describe('Bubble Sort', function(){
+
+  beforeEach(function() {
+    spyOn(toLower, 'call').and.callThrough();
+  });
+
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
+
+  it('sorts a one-element array', function(){
+    var arr = [1];
+    expect(bubbleSort(arr)).toEqual(arr);
+  });
+
+  it('sorts a longer array', function(){
+    var arr = [3,1,2];
+    expect(bubbleSort(arr)).toEqual([1,2,3]);
+  });
+
+  it('executes the expected number of times', function(){
+    var arr = [3,1,2];
+    expect(toLower.calls.count()).toEqual(4);
+  }
 });
+
+
+
